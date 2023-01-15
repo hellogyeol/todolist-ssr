5080092bf459d7f476e08201612f3e733886db38
hellogyeol <hellogyeol@gmail.com>
2023-01-15 17:48:23 +0900

    server: POST 요청 시 form 입력값으로 ToDo 추가

--- server.js
+++ server.js
@@ -8,10 +8,28 @@ const url = process.env.DB_URL;
 const client = new MongoClient(url);
 
 app.set('view engine', 'pug');
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.get('/', async (req, res) => {
   await client.connect();
   const col = client.db('ssrDb').collection('ssrCol');
+
+  const todoList = await col.find({}).toArray();
+  console.log(todoList);
+
+  res.render('index', {
+    todoList: todoList
+  });
+});
+
+app.post('/', async (req, res) => {
+  await client.connect();
+  const col = client.db('ssrDb').collection('ssrCol');
+
+  await col.insertOne({
+    content: req.body.content
+  });
 
   const todoList = await col.find({}).toArray();
   console.log(todoList);
